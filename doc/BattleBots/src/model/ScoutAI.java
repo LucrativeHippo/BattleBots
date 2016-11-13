7f619e8045f6240cf4038b49bb74fadc936b1d1d
William Revell <wjr992@drone24.usask.ca>
2016-11-13 17:20:48 -0600

    added to the AI move command

--- doc/BattleBots/src/model/ScoutAI.java
+++ doc/BattleBots/src/model/ScoutAI.java
@@ -22,24 +22,52 @@ public class ScoutAI extends Scout {
         public void move(){
             if(this.getMovementLeft()>0){
             if(this.getRelativeDirection()==0){
-                if(){
-                    
+                if(this.board.spaces[this.getHorizontalLocation()+1][this.getVerticalLocation()]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()+1]][this.getVerticalLocation()].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation()+1);
+                    this.setVerticalLocation(this.getVerticalLocation());
                 }
             }
             if(this.getRelativeDirection()==1){
-                
+                if(this.board.spaces[this.getHorizontalLocation()][this.getVerticalLocation()+1]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()+1].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation());
+                    this.setVerticalLocation(this.getVerticalLocation()+1);
+                }
             }
             if(this.getRelativeDirection()==2){
-                
+                if(this.board.spaces[this.getHorizontalLocation()-1][this.getVerticalLocation()+1]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()-1]][this.getVerticalLocation()+1].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation()-1);
+                    this.setVerticalLocation(this.getVerticalLocation()+1);
+                }
             }
             if(this.getRelativeDirection()==3){
-                
+                if(this.board.spaces[this.getHorizontalLocation()-1][this.getVerticalLocation()]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()-1]][this.getVerticalLocation()].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation()-1);
+                    this.setVerticalLocation(this.getVerticalLocation());
+                }
             }
             if(this.getRelativeDirection()==4){
-                
+                if(this.board.spaces[this.getHorizontalLocation()][this.getVerticalLocation()-1]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()-1].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation());
+                    this.setVerticalLocation(this.getVerticalLocation()-1);
+                }
             }
             if(this.getRelativeDirection()==5){
-                
+                if(this.board.spaces[this.getHorizontalLocation()+1][this.getVerticalLocation()-1]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()+1]][this.getVerticalLocation()-1].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation()+1);
+                    this.setVerticalLocation(this.getVerticalLocation()-1);
+                }
             }
             }
         }
@@ -49,8 +77,10 @@ public class ScoutAI extends Scout {
 		
 	}
 	
-	public void turn(){
-		
+	public void turn(int desiredDirection){
+		if((desiredDirection<=5)&&(desiredDirection>=0)){
+                    this.setRelativeDirection((this.getRelativeDirection()+desiredDirection)%6);
+                }
 	}
 
 }
--- doc/BattleBots/src/model/TankAI.java
+++ doc/BattleBots/src/model/TankAI.java
@@ -19,12 +19,66 @@ public class TankAI extends Tank{
             
 	}
 	
+	public void move(){
+            if(this.getMovementLeft()>0){
+            if(this.getRelativeDirection()==0){
+                if(this.board.spaces[this.getHorizontalLocation()+1][this.getVerticalLocation()]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()+1]][this.getVerticalLocation()].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation()+1);
+                    this.setVerticalLocation(this.getVerticalLocation());
+                }
+            }
+            if(this.getRelativeDirection()==1){
+                if(this.board.spaces[this.getHorizontalLocation()][this.getVerticalLocation()+1]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()+1].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation());
+                    this.setVerticalLocation(this.getVerticalLocation()+1);
+                }
+            }
+            if(this.getRelativeDirection()==2){
+                if(this.board.spaces[this.getHorizontalLocation()-1][this.getVerticalLocation()+1]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()-1]][this.getVerticalLocation()+1].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation()-1);
+                    this.setVerticalLocation(this.getVerticalLocation()+1);
+                }
+            }
+            if(this.getRelativeDirection()==3){
+                if(this.board.spaces[this.getHorizontalLocation()-1][this.getVerticalLocation()]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()-1]][this.getVerticalLocation()].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation()-1);
+                    this.setVerticalLocation(this.getVerticalLocation());
+                }
+            }
+            if(this.getRelativeDirection()==4){
+                if(this.board.spaces[this.getHorizontalLocation()][this.getVerticalLocation()-1]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()-1].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation());
+                    this.setVerticalLocation(this.getVerticalLocation()-1);
+                }
+            }
+            if(this.getRelativeDirection()==5){
+                if(this.board.spaces[this.getHorizontalLocation()+1][this.getVerticalLocation()-1]!=null){
+                    //this.board.spaces[this.getHorizontalLocation()]][this.getVerticalLocation()].robotList.remove(this.name);
+                    //this.board.spaces[this.getHorizontalLocation()+1]][this.getVerticalLocation()-1].robotList.add(this);
+                    this.setHorizontalLocation(this.getHorizontalLocation()+1);
+                    this.setVerticalLocation(this.getVerticalLocation()-1);
+                }
+            }
+        }
+        
+        
 	public void scan(){
 		
 	}
 	
-	public void turn(){
-		
+	public void turn(int desiredDirection){
+		if((desiredDirection<=5)&&(desiredDirection>=0)){
+                    this.setRelativeDirection((this.getRelativeDirection()+desiredDirection)%6);
+                }
 	}
-	
 }
