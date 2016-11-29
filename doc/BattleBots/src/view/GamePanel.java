a157ba4ca29efdf973dc92d370397e51e0c67e2f
Haotian Ma <ham896@drone20.usask.ca>
2016-11-29 16:21:37 -0600

    fixed bug with stacking permanently

--- doc/BattleBots/src/view/GamePanel.java
+++ doc/BattleBots/src/view/GamePanel.java
@@ -222,9 +222,9 @@ public class GamePanel extends JPanel implements GameObserver{
 				//shoot instead of getting rid of space
                                 if (!gameBoard.spaces[p.x][p.y].robotList.isEmpty()){
                                     gameinfo.getCurrentRobot().shoot(gameBoard.spaces[p.x][p.y]);
-                                    
-                                        while(!gameBoard.spaces[p.x][p.y].isEmpty()){
-                                          Iterator<Robot> robots = gameBoard.spaces[p.x][p.y].robotList.iterator();
+                                    Iterator<Robot> robots = gameBoard.spaces[p.x][p.y].robotList.iterator();
+                                        while(!gameBoard.spaces[p.x][p.y].isEmpty() && robots.hasNext()){
+                                          
                                             System.out.println("robot on hex");
                                             if (!robots.hasNext()){
                                                 break;
@@ -232,7 +232,12 @@ public class GamePanel extends JPanel implements GameObserver{
                                             Robot temp = robots.next();
                                             if (temp.getHealthLeft()<= 0){
                                                 gameBoard.spaces[p.x][p.y].robotList.remove(temp);
+                                                robots = gameBoard.spaces[p.x][p.y].robotList.iterator();
                                             System.out.println("robot removed");
+                                            }
+                                            else
+                                            {
+                                                
                                             }
                                         }
 				repaint();
